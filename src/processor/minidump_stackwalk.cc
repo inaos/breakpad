6a85ce55cdbc4506f10fa8584a7503b0c28576ef
Christian Steiner <christian.steiner@inaos.ch>
2019-03-09 21:55:43 +0100

    adding getopt for windows

--- src/processor/minidump_stackwalk.cc
+++ src/processor/minidump_stackwalk.cc
@@ -34,7 +34,10 @@
 
 #include <stdio.h>
 #include <string.h>
+
+#ifndef WIN32
 #include <unistd.h>
+#endif
 
 #include <limits>
 #include <string>
@@ -51,6 +54,79 @@
 #include "processor/simple_symbol_supplier.h"
 #include "processor/stackwalk_common.h"
 
+#ifdef WIN32
+#include <string.h>
+#include <stdio.h>
+
+int     opterr = 1,             /* if error message should be printed */
+        optind = 1,             /* index into parent argv vector */
+        optopt,                 /* character checked for validity */
+        optreset;               /* reset getopt */
+char    *optarg;                /* argument associated with option */
+
+#define BADCH   (int)'?'
+#define BADARG  (int)':'
+#define EMSG    ""
+
+/*
+ * getopt --
+ *      Parse argc/argv argument vector.
+ */
+int getopt(int nargc, char * const nargv[], const char *ostr)
+{
+  static char *place = EMSG;              /* option letter processing */
+  const char *oli;                              /* option letter list index */
+
+  if (optreset || !*place) {              /* update scanning pointer */
+    optreset = 0;
+    if (optind >= nargc || *(place = nargv[optind]) != '-') {
+      place = EMSG;
+      return (-1);
+    }
+    if (place[1] && *++place == '-') {      /* found "--" */
+      ++optind;
+      place = EMSG;
+      return (-1);
+    }
+  }                                       /* option letter okay? */
+  if ((optopt = (int)*place++) == (int)':' ||
+    !(oli = strchr(ostr, optopt))) {
+      /*
+      * if the user didn't specify '-' as an option,
+      * assume it means -1.
+      */
+      if (optopt == (int)'-')
+        return (-1);
+      if (!*place)
+        ++optind;
+      if (opterr && *ostr != ':')
+        (void)printf("illegal option -- %c\n", optopt);
+      return (BADCH);
+  }
+  if (*++oli != ':') {                    /* don't need argument */
+    optarg = NULL;
+    if (!*place)
+      ++optind;
+  }
+  else {                                  /* need an argument */
+    if (*place)                     /* no white space */
+      optarg = place;
+    else if (nargc <= ++optind) {   /* no arg */
+      place = EMSG;
+      if (*ostr == ':')
+        return (BADARG);
+      if (opterr)
+        (void)printf("option requires an argument -- %c\n", optopt);
+      return (BADCH);
+    }
+    else                            /* white space */
+      optarg = nargv[optind];
+    place = EMSG;
+    ++optind;
+  }
+  return (optopt);                        /* dump back option letter */
+}
+#endif
 
 namespace {
 
@@ -120,6 +196,7 @@ bool PrintMinidumpProcess(const Options& options) {
 }  // namespace
 
 static void Usage(int argc, const char *argv[], bool error) {
+  std::string bn(argv[0]);
   fprintf(error ? stderr : stdout,
           "Usage: %s [options] <minidump-file> [symbol-path ...]\n"
           "\n"
@@ -129,7 +206,7 @@ static void Usage(int argc, const char *argv[], bool error) {
           "\n"
           "  -m         Output in machine-readable format\n"
           "  -s         Output stack contents\n",
-          google_breakpad::BaseName(argv[0]).c_str());
+          bn.c_str());
 }
 
 static void SetupOptions(int argc, const char *argv[], Options* options) {
